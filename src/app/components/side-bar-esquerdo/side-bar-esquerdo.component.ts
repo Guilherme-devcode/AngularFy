7bec188b31db7f5789778d2c9b555ec95c4c84d6
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2022-03-02 11:13:45 -0300

    Criando tela de play

---
+++ src/app/components/side-bar-esquerdo/side-bar-esquerdo.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { IPlaylist } from 'src/app/Interfaces/IPlaylist';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+  selector: 'app-side-bar-esquerdo',
+  templateUrl: './side-bar-esquerdo.component.html',
+  styleUrls: ['./side-bar-esquerdo.component.scss']
+})
+export class SideBarEsquerdoComponent implements OnInit {
+
+  menuSelecionado = 'Home';
+  playlists: IPlaylist[] = [];
+
+  //Icones
+  homeIcone = faHome
+  pesquisarIcone = faSearch
+  artistaIcone = faGuitar
+  playlistIcone = faMusic 
+
+  constructor(
+    private router: Router,
+    private spotifyService: SpotifyService) { }
+
+  ngOnInit(): void {
+    this.buscarPlaylists()
+  }
+
+  botaoClick(botao: string){
+    this.menuSelecionado = botao
+    this.router.navigateByUrl('player/home')
+  }
+
+  async buscarPlaylists(){
+    this.playlists = await this.spotifyService.buscarPlaylistUsuario()
+  }
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
   production: true
 };
+export const SpotifyConfiguration = {
+  clientId: '504162c685064f7083439c7ade595054',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
